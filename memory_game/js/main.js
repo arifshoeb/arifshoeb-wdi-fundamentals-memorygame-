b88177cafeaa0c418f4c590037939f5fef86da11
Arif Shoeb <arifshoeb@Arifs-iMac.local>
2019-07-15 17:44:12 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,13 +1,22 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards [0];
-cardsInPlay.push("cardOne");
-console.log("User Flipped Queen");
-var cardTwo = cards [2];
-cardsInPlay.push(cardTwo);
-console.log("User Flipped King");
+
+function checkForMatch(); {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
+
+function flipCard(cardId); {
+	var flipCard === 0;
+	var flipCard === 2;
+console.log("User flipped" + cards[cardId])
+cardsInPlay.push(cards[cardId]);
 if (cardsInPlay.length === 2 && cardsInPlay [] === cardsInPlay []) {
 alert("You Found a Match");}
 else if (cardsInPlay.length === 2 && cardsInPlay [] !== cardsInPlay [])
 {alert("Sorry Try Again");
+}	
 }
